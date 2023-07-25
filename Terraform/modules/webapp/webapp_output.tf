3f664d9127a822625fba1b4d6ccef2ea3cab8932
Surendra Reddy Dasari <122847551+SurendraReddyDasari@users.noreply.github.com>
2023-07-25 21:56:33 +0100

    Update webapp_output.tf

--- Terraform/modules/webapp/webapp_output.tf
+++ Terraform/modules/webapp/webapp_output.tf
@@ -1,4 +1,4 @@
 output "web_app_url" {
   description = "URL of the deployed Web App."
-  value       = azurerm_linux_web_app.example.default_hostname
+  value       = azurerm_windows_web_app.example.default_hostname
 }
